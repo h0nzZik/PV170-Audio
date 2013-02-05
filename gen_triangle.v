e4884a8cd678b38e8fea72996c93ac2304e4c19d
Jan Tusil <xtusil@fi.muni.cz>
2013-02-05 19:59:44 +0100

    better

--- gen_triangle.v
+++ gen_triangle.v
@@ -10,16 +10,16 @@ module gen_triangle (
 );
 
 
-input	[15:0]	freq;			// 1 to 65_535
-input	[31:0]	sys_clk_freq;	// >= 1 MHz
-input			sys_clk;
+input		[13:0]	freq;				// 1 Hz to 16 kHz
+input		[31:0]	sys_clk_freq;	// > 1 MHz (>= 2^20 Hz)
+input					sys_clk;
 
 output	[23:0]	out;
 reg		[23:0]	out;
 
-input	[7:0]	duty;			// 1 to 255
+input		[7:0]		duty;				// 1 to 255
 output	[31:0]	debug_out;
-output			debug_led;
+output				debug_led;
 
 // generate 'data_reset' clock
 
@@ -33,7 +33,7 @@ gen_clock data_reset_clock
 );
 
 
-
+/*
 // sys_clk_freq at least 2^20 Hz (sizeof(tmp) - no_bits(sys_clock_freq) + 1)))
 // 2x faster
 wire [20:0] increment;
@@ -46,21 +46,35 @@ reg [31:0] time_elapsed;
 assign debug_out = decrement;
 
 
-wire	[47:0]	big_f;	// 16 bits == frequency,
+wire	[45:0]	big_f;	// 14 bits == frequency,
 						// 24 bits == output range, 
 						// 8  bits == duty
 
 assign big_f = (freq << (24+8)) / sys_clk_freq;
-assign increment = (duty != 0) ? big_f / duty : 0;
+assign increment = (duty != 0) ? (big_f / duty) : 0;
 assign decrement = big_f / (256 - duty);
 
 wire	[39:0]	duty_freq;
 assign duty_freq = sys_clk_freq * duty;
 assign time_to_change = duty_freq / (256 * freq);
 
+*/
 
+/* new and better */
+wire	[31:0]	ticks_per_period;
+wire	[31:0]	ticks_up;
+wire	[31:0]	ticks_dn;
+reg	[31:0]	ticks_elapsed = 0;
 
+assign ticks_per_period = sys_clk_freq / freq;
+assign ticks_up	= duty * (ticks_per_period >> 4'h8);
+assign ticks_dn	= ticks_per_period - ticks_up;
 
+wire [23:0] increment;
+wire [23:0] decrement;
+
+assign increment = (ticks_up == 32'h0)? 25'h0: 25'h1000000 / ticks_up;
+assign decrement = (ticks_dn == 32'h0)? 25'h0: 25'h1000000 / ticks_dn;
 
 reg last_reset=0;
 always@(posedge sys_clk)
@@ -69,20 +83,19 @@ begin
 	if ((data_reset == 1) && (last_reset == 0))
 	begin
 		out <= 0;
-		time_elapsed <= 0;
+		ticks_elapsed <= 0;
 	end
 	else
 	begin
-		if (time_elapsed < time_to_change)
+		if (ticks_elapsed < ticks_up)
 		begin
 			out <= out + increment;
-			time_elapsed <= time_elapsed + 1;
+			ticks_elapsed <= ticks_elapsed + 1;
 		end
 		else
 		begin
 			out <= out - decrement;
 		end
-		//out <= out + increment;
 	end
 	last_reset <= data_reset;
 end
